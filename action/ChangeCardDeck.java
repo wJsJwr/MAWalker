f36cb5d38ca6f5c96743a367f8e32e072f66e45a
MengHang <chocolate.menghang@gmail.com>
2013-10-02 23:56:33 +0800

    Fix change card deck when there are only two cards,and a better way of finding a leader.

--- action/ChangeCardDeck.java
+++ action/ChangeCardDeck.java
@@ -30,10 +30,19 @@ public class ChangeCardDeck {
 		else
 			Info.LastDeckNo = Process.info.CurrentDeck.No;
 
-		Document doc;
 		ArrayList<NameValuePair> post = new ArrayList<NameValuePair>();
 		post.add(new BasicNameValuePair("C", Process.info.CurrentDeck.card));
-		post.add(new BasicNameValuePair("lr", Process.info.CurrentDeck.leader));
+		String[] tmpCard = Info.MyDeckA1.card.split(",");
+		int cards = 0;
+		for (int i = 0; i < tmpCard.length; i++) {
+			if (!tmpCard[i].equals("empty")) {
+				cards++;
+			}
+		}
+		if (cards != 2) {
+			post.add(new BasicNameValuePair("lr",
+					Process.info.CurrentDeck.leader));
+		}
 		post.add(new BasicNameValuePair("no", "2"));
 		try {
 			response = walker.Process.network.ConnectToServer(
@@ -54,6 +63,7 @@ public class ChangeCardDeck {
 			outputFileStream.close();
 		}
 
+		Document doc;
 		try {
 			doc = Process.ParseXMLBytes(response);
 		} catch (Exception ex) {
