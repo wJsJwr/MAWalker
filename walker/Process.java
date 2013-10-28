08776846cee33b6dafa30d95dcd185a305ae0b49
netfishx <netfishx@gmail.com>
2013-10-28 14:51:46 +0800

    fix bug

--- walker/Process.java
+++ walker/Process.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
